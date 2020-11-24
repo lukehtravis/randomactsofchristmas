f997af1293c5e28dcced0391e0bdb76dddefa482
Luke Travis <lukehtravis@gmail.com>
2020-11-23 21:27:06 -0800

    feat: site with picture

--- src/App.js
+++ src/App.js
@@ -1,22 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import tree from "./tree.jpg";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={tree} alt="Random Acts of Chrismas" />
+        <p>Random Acts Of Christmas. Coming Soon To A Block Near You</p>
       </header>
     </div>
   );
